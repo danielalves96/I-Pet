61630e62d0e3a2ee63c9a842e14a2d5592fc66e8
Daniel <daniel@clinicarx.com.br>
2020-11-04 13:38:42 -0300

    IPET-0006 Ajustes para inclusão de campos novos no backend

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -2,12 +2,14 @@ import Knex from 'knex';
 
 export async function seed(knex: Knex) {
     await knex('items').insert([
-        { title: 'Clínicas', image: 'clinic.svg'},
+        { title: 'Hospital PET', image: 'hospital.svg'},
+        { title: 'Clínica PET', image: 'clinic.svg'},
         { title: 'Pet Shop', image: 'shop.svg'},
-        { title: 'Hotel PET', image: 'hostel.svg'},
         { title: 'Lojas', image: 'store.svg'},
+        { title: 'Aviários', image: 'aviary.svg'},
+        { title: 'Vacinação', image: 'vaccine.svg'},
+        { title: 'Hotel PET', image: 'hostel.svg'},
         { title: 'ONGs', image: 'ong.svg'},
         { title: 'Pontos de adoção', image: 'adoption.svg'},
     ])
-
 }
