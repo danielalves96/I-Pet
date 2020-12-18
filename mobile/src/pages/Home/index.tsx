e38b91a54eb9df561cebb639fb7a2005f34922fa
Daniel <daniel@clinicarx.com.br>
2020-12-18 14:14:51 -0300

    IPET-0030 FirstCommit real do mobile

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,32 @@
+import React  from 'react';
+import { View, ImageBackground, Image, Text } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+import { RectButton } from 'react-native-gesture-handler';
+import { Translate } from '../../Internacionalization/PT_BR';
+import { styles } from './style';
+
+const Home = () => {
+
+    return (
+        <ImageBackground source={require('../../assets/pets.png')} style={styles.container} imageStyle={{ width: 314, height: 314, marginTop: 100, marginLeft: 40 }}>
+            <View style={styles.main}>
+                <Image source={require('../../assets/logo.png')} />
+                <Text style={styles.title}>{ Translate.map(app => app.Home.TITLE_HOME) }</Text>
+                <Text style={styles.description}>{ Translate.map(app => app.Home.DESCRIPTION) }</Text>
+            </View>
+
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={() => { }}>
+                    <View style={styles.buttonIcon}>
+                        <Text >
+                            <AntDesign name="arrowright" size={24} color="#FFF" />
+                        </Text>
+                    </View>
+                    <Text style={styles.buttonText} >{ Translate.map(app => app.Home.FIND) }</Text>
+                </RectButton>
+            </View>
+        </ImageBackground>
+    )
+};
+
+export default Home;
